2d78f5909926e940e0f66886c39a16bb18fd19da
unknown <g@bitdiff.com>
2019-08-09 10:50:23 +0100

    m

--- src/Bitdiff.Utils/Cookie.cs
+++ src/Bitdiff.Utils/Cookie.cs
@@ -6,27 +6,15 @@ namespace Bitdiff.Utils
     public class Cookie : ICookie
     {
         private readonly IHttpContextFactory _contextFactory;
+        public static string DefaultDomain;
 
         public Cookie(IHttpContextFactory contextFactory)
         {
             _contextFactory = contextFactory;
         }
 
-        private HttpCookieCollection RequestCookies
-        {
-            get
-            {
-                return _contextFactory.GetContext().Request.Cookies;
-            }
-        }
-
-        private HttpCookieCollection ResponseCookies
-        {
-            get
-            {
-                return _contextFactory.GetContext().Response.Cookies;
-            }
-        }
+        private HttpCookieCollection RequestCookies => _contextFactory.GetContext().Request.Cookies;
+        private HttpCookieCollection ResponseCookies => _contextFactory.GetContext().Response.Cookies;
 
         private HttpCookie GetCookieInItems(string name)
         {
@@ -40,13 +28,19 @@ namespace Bitdiff.Utils
 
         public string Get(string name)
         {
-            HttpCookie cookie = GetCookieInItems(name) ?? RequestCookies[name] ?? ResponseCookies[name];
-            return cookie != null ? cookie.Value : null;
+            var cookie = GetCookieInItems(name) ?? RequestCookies[name] ?? ResponseCookies[name];
+            return cookie?.Value;
         }
 
         public void Set(string name, string value)
         {
             var httpCookie = new HttpCookie(name, value);
+
+            if (DefaultDomain.HasValue())
+            {
+                httpCookie.Domain = DefaultDomain;
+            }
+
             ResponseCookies.Add(httpCookie);
             SetCookieInItems(httpCookie);
         }
@@ -54,6 +48,12 @@ namespace Bitdiff.Utils
         public void Set(string name, string value, DateTime expires)
         {
             var httpCookie = new HttpCookie(name, value) { Expires = expires };
+
+            if (DefaultDomain.HasValue())
+            {
+                httpCookie.Domain = DefaultDomain;
+            }
+
             ResponseCookies.Add(httpCookie);
             SetCookieInItems(httpCookie);
         }
--- src/Bitdiff.Utils/Properties/AssemblyInfo.cs
+++ src/Bitdiff.Utils/Properties/AssemblyInfo.cs
@@ -4,5 +4,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("Bit Diffusion")]
 [assembly: ComVisible(false)]
 [assembly: Guid("70568cf1-837c-4a42-af0e-a8cf174dd98a")]
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
